5a856e9fe79147595c1e09052c2beacc04ec50d1
Ricardo <ricardosneres22@gmail.com>
2026-07-24 17:01:01 -0300

    Criação da classe 'Usuario'

---
+++ Models/usuario_model.py
@@ -0,0 +1,48 @@
+from uuid import uuid4
+
+from Models.device_model import Dispositivo
+
+class Usuario:
+    def __init__(self, nome : str, email : str):
+        self.nome = nome
+        self.email = email
+        self.__dispositivos_salvos : list[Dispositivo] = []
+        self.__identidade = uuid4()
+
+    @property
+    def nome(self) -> str:
+        return self.__nome
+    @property
+    def email(self) -> str:
+        return self.__email
+
+    @nome.setter
+    def nome(self, entrada):
+        if not isinstance(entrada, str):
+            raise TypeError("O nome deve ser uma string")
+
+        entrada_limpa = entrada.strip()
+        if not entrada_limpa:
+            raise ValueError("O nome não pode estar vazio")
+
+        self.__nome = entrada_limpa
+
+    @email.setter
+    def email(self, entrada):
+        if not isinstance(entrada, str):
+            raise TypeError("O email deve ser uma string")
+
+        entrada_limpa = entrada.strip()
+        if not entrada_limpa:
+            raise ValueError("O email não pode estar vazio")
+
+        if not entrada_limpa.endswith("@gmail.com"):
+            raise ValueError("O email deve conter o dominio '@gmail.com'")
+        self.__email = entrada_limpa
+
+    @property
+    def dispositivos_salvos(self) -> list[Dispositivo]:
+        return self.__dispositivos_salvos.copy()
+
+    def quantidade_dispositivos_salvos(self) -> int:
+        return len(self.__dispositivos_salvos)
